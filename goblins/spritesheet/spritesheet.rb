81200e11feee354eed819d9dd88482274724ee2f
Gerald Bauer <gerald.bauer@gmail.com>
2022-06-04 16:37:00 +0200

    up

--- goblins/spritesheet/spritesheet.rb
+++ goblins/spritesheet/spritesheet.rb
@@ -5,48 +5,57 @@ require 'pixelart'
 
 
 
-attributes = CsvHash.read( "./attributes-24x24.csv")
-puts "  #{attributes.size} record(s)"  #=> ??
+def generate_spritesheet( path,
+                          cols: 10,
+                          dir: '.' )
+
+   ## check - change dir to image_dir or such - why? why not?
+
+   attributes = CsvHash.read( path )
+   puts "  #{attributes.size} record(s)"  #=> ??
 
 
-total = attributes.size
+   total = attributes.size
 
-cols = 10
-rows = total/10
-rows += 1  if (total % 10) != 0     ## add extra row for remainder
+   rows = total/10
+   rows += 1  if (total % 10) != 0     ## add extra row for remainder
 
-sheet = ImageComposite.new( cols, rows )
+   sheet = ImageComposite.new( cols, rows )
 
-
-meta = []  ## output meta(data) records
+   meta = []  ## output meta(data) records
 
 
-####
-#  add readymades
-attributes.each do |rec|
-  path = rec['path']
-  sheet << Image.read( "./i/24x24/#{path}" )
+   ####
+   #  add attributes
+   attributes.each do |rec|
+     img_path = rec['path']
+     sheet << Image.read( "#{dir}/#{img_path}" )
 
-  name       = rec['name']
-  more_names = (rec['more_names'] || '').split( '|' )
+     name       = rec['name']
+     more_names = (rec['more_names'] || '').split( '|' )
 
-  if name.nil? || name.empty?
-      name = File.basename( path, File.extname( path ))
-  end
+     if name.nil? || name.empty?
+        name = File.basename( img_path, File.extname( img_path ))
+     end
 
+     ## normalize spaces in more names
+     names = [name] + more_names
+     names = names.map {|str| str.strip.gsub(/[ ]{2,}/, ' ' )}
 
-  ## normalize spaces in more names
-  names = [name] + more_names
-  names = names.map {|str| str.strip.gsub(/[ ]{2,}/, ' ' )}
+     meta << [meta.size,
+              rec['category'],
+              names[0],
+              names[1..-1].join( ' | '),
+             ]
+    end
 
-  meta << [meta.size,
-           rec['category'],
-           names[0],
-           names[1..-1].join( ' | '),
-         ]
+    [sheet,meta]
 end
 
 
+
+sheet, meta = generate_spritesheet( './attributes-24x24.csv',
+                                    dir: './i/24x24' )
 
 sheet.save( "./tmp/spritesheet-24x24.png" )
 sheet.zoom(2).save( "./tmp/spritesheet-24x24@2x.png" )
@@ -62,5 +71,24 @@ File.open( "./tmp/spritesheet-24x24.csv", "w:utf-8" ) do |f|
      f.write( "\n" )
    end
 end
+
+
+sheet, meta = generate_spritesheet( './attributes-20x20.csv',
+                                    dir: './i/20x20' )
+
+sheet.save( "./tmp/spritesheet-20x20.png" )
+sheet.zoom(2).save( "./tmp/spritesheet-20x20@2x.png" )
+sheet.zoom(4).save( "./tmp/spritesheet-20x20@4x.png" )
+
+headers = ['id', 'category', 'name', 'more_names']
+File.open( "./tmp/spritesheet-20x20.csv", "w:utf-8" ) do |f|
+   f.write( headers.join( ', '))
+   f.write( "\n")
+   meta.each do |values|
+     f.write( values.join( ', ' ))
+     f.write( "\n" )
+   end
+end
+
 
 puts "bye"
