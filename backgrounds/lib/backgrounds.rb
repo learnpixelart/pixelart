36ffddfb96687efb17a6ba4a59164a23d7e84a78
Gerald Bauer <gerald.bauer@gmail.com>
2022-06-08 14:28:43 +0200

    up

--- backgrounds/lib/backgrounds.rb
+++ backgrounds/lib/backgrounds.rb
@@ -17,6 +17,53 @@ end
 
 module Backgrounds
 
+class Image
+  def self.generate( *values,
+                       width:,
+                       height: )
+       img = new( width, height )
+       img = do_parse( img, *values )
+       img
+  end # method Image.generate
+
+  ##
+  ##  todo - what syntax to use for stripe/stripes ??
+  ##                         or for gradient/gradients ??
+  ##  stripe( color1 color2  )  ??
+  ##  gradient( color1 color2 )
+  def self.do_parse( img, *values )
+      head = values[0]
+      more = values[1..-1]
+
+      while head do
+         img = do_generate( img, head )
+
+         head = more[0]
+         more = more[1..-1]
+      end
+      img
+  end
+
+  def self.do_generate( img, value )
+    if value.is_a?( String )
+      if ['ua','ukraine'].include?( value.downcase )
+         img.ukraine
+      elsif ['pride','rainbow'].include?( value.downcase )
+         img.pride
+      else  ### assume color
+        img.compose!( Image.new( img.width, img.height, value ) )
+        img
+      end
+    elsif value.is_a?( Pixelart::Image )
+      img.compose!( value )
+      img
+    else ###  assume integer  (assert) - why? why not
+      img.compose!( Image.new( img.width, img.height, value ) )
+      img
+    end
+  end
+
+end # class Image
 end #  module Backgrounds
 
 
@@ -26,10 +73,18 @@ Background  = Backgrounds
 
 
 
+module Pixelart
+class Image
+  def background( *values )
+    img = Backgrounds::Image.generate( *values,
+                                        width:  @img.width,
+                                        height: @img.height )
+    img.compose!( self )
+    img    # note: return new image
+  end
+end  # class Image
+end # module Pixelart
 
-###
-# note: for convenience auto include Pixelart namespace!!! - why? why not?
-include Pixelart
 
 
 
---
+++ backgrounds/sandbox/test_generate.rb
@@ -0,0 +1,32 @@
+###
+#  to run use
+#     ruby -I ./lib sandbox/test_generate.rb
+
+
+require 'backgrounds'
+
+
+rainbow1 = Pixelart::Image.read( './i/rainbow1-24x24.png' )
+rainbow2 = Pixelart::Image.read( './i/rainbow2-24x24.png' )
+
+
+backgrounds = [
+  ['0xff0000'],    ## red
+  ['ukraine'],
+  ['pride'],
+  ['0x0000ff', rainbow1],
+  ['ukraine', rainbow2],
+]
+
+backgrounds.each_with_index do |values, i|
+  bg = Background::Image.generate( *values, width: 24, height: 24 )
+  bg.save( "./tmp/bg#{i}.png" )
+  bg.zoom(4).save( "./tmp/bg#{i}@4x.png" )
+
+  bg = Pixelart::Image.new( 24, 24 ).background( *values )
+  bg.save( "./tmp/bg#{i}b.png" )
+  bg.zoom(4).save( "./tmp/bg#{i}b@4x.png" )
+end
+
+
+puts "bye"
